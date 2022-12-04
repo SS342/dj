efd0d1ef2471bb7cbb3e5cb2e1271832fba66dd7
SS342 <alex2005ov@gmail.com>
2022-12-04 11:04:30 +0300

    1

--- 2/allelleo/blog/models.py
+++ 2/allelleo/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -9,6 +10,9 @@ class Blog(models.Model):
     time_created = models.DateTimeField(auto_now_add=True)
     time_updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
 
     def __str__(self):
         return f"[ {self.id} ] : {self.title}"
